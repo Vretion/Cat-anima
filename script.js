296cc7dd89816fbe3e2baaf728cc8c84f743c6a8
Vretion <abdulsukadana1234@gmail.com>
2024-08-20 11:21:23 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// Informasi Pembuat
+document.getElementById('info').innerHTML = `
+    <p>Pelopor Pembuat dan Pencipta:</p>
+    <p>Nama: Abdul Sukur</p>
+    <p>Email: sukurabdul380@gmail.com</p>
+`;
